f9f5e4eaee25de1a21e703fd60871afab00bb5b5
Sammako <marie.aurelle@free.fr>
2016-04-18 16:29:24 +0200

    Maj makefile

--- chiffrement.h
+++ chiffrement.h
@@ -1,5 +1,4 @@
-#include <conversion.h>
-char boite_S[16] = {'b', 'f', '3', '2', 'a', 'c', '9', '1', '6', '7', '8', '0', 'e', '5', 'd', '4'};
+#include "conversion.h"
 
 void substitution(int bin[4]);
 void permutation(int Etat[taille_msg_hexa*4]);
