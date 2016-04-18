422da983e11a7d1686564933f7884c5d54a582e3
Sammako <marie.aurelle@free.fr>
2016-04-18 16:25:43 +0200

    Maj makefile

--- conversion.c
+++ conversion.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "conversion.h"
 #define taille_msg_hexa 16
 #define taille_cle_hexa 20
 char hexa[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
