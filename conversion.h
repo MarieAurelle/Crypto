1bba4ea7171522c65c9f6707734530b1316031ab
Sammako <marie.aurelle@free.fr>
2016-04-18 16:27:22 +0200

    Maj Makefile

--- conversion.h
+++ conversion.h
@@ -3,7 +3,6 @@
 #include <math.h>
 #define taille_msg_hexa 16
 #define taille_cle_hexa 20
-char hexa[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
 void conversion_binaire(char hexa, int binaire[4], int deb);
 char conversion_hexadecimal(int bin[4]);
